dc0f68be028e142632805e9892d45f50ba66db90
chathunga2007 <wggachathungabimsara2007@gmail.com>
2026-03-11 10:11:02 +0530

    initial commit

---
+++ JavaScript Basics/Introduction/Example 02/script.js
@@ -0,0 +1,14 @@
+function showMessage() {
+    alert("Hello, Students!");
+}
+
+function textChange() {
+    document.getElementById("text").innerHTML = "Welcome to JavaScript!";
+}
+
+function calculateSum() {
+    var num1 = 10;
+    var num2 = 5;
+    var sum = num1 + num2;
+    alert("The sum is: " + sum);
+}
